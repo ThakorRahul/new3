08c6bd77e55d4839cd6e4540885ef519f969f857
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-02-21 17:22:20 +0530

    Update boobs.py

--- tg_bot/modules/boobs.py
+++ tg_bot/modules/boobs.py
@@ -20,6 +20,5 @@ def boobs(bot: Bot, update: Update):
     nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
     final = "http://media.oboobs.ru/{}".format(nsfw)
     update.message.reply_photo(final)
-		
-BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs, admin_ok=True)
+		BOOBS_HANDLER = DisableAbleCommandHandler("boobs", boobs, filters=CustomFilters.sudo_filter)
 dispatcher.add_handler(BOOBS_HANDLER)
