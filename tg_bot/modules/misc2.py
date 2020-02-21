f5b68a9bc16f02c18047432d661f71a172fdd3ab
shreyansh8184 <54529384+shreyansh8184@users.noreply.github.com>
2020-02-21 18:25:33 +0530

    Update misc2.py

--- tg_bot/modules/misc2.py
+++ tg_bot/modules/misc2.py
@@ -13,7 +13,7 @@ from telegram import ParseMode
 from telegram.ext import CommandHandler, run_async, Filters
 from telegram.utils.helpers import escape_markdown, mention_html
 from tg_bot.modules.helper_funcs.chat_status import user_admin, sudo_plus
-from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, DEV_USERS, WHITELIST_USERS, BAN_STICKER
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
 from tg_bot.__main__ import STATS, USER_INFO, TOKEN
 from tg_bot.modules.disable import DisableAbleCommandHandler, DisableAbleRegexHandler
 from tg_bot.modules.helper_funcs.extraction import extract_user
